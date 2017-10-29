52dfa1b7d3d27e3ffca699ce4d0fdeefbc880e7a
Patrick Sollars <pjsollars@gmail.com>
2017-10-29 13:23:56 -0400

    pretty styles and code cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
-	
-const flipButton = document.getElementById("flip");
-let coin = ["",""]
-const messageBox = document.getElementById("messageBox");
-const tails = document.getElementById("tails");
-const heads = document.getElementById("heads");
-const coinSpin = document.getElementById("spin");
-//const userGuess = document.getElementById("guess");
+(function(){
+	const flipButton = document.getElementById("flip");
+	let coin = ["",""]
+	const messageBox = document.getElementById("messageBox");
+	const tails = document.getElementById("tails");
+	const heads = document.getElementById("heads");
+	const coinSpin = document.getElementById("spin");
 
-tails.onclick = function() {
-	flipACoin();
-	checkGuess("tails", coin[0]);
-	animation(coin);
-}
+	tails.onclick = function() {
+		flipACoin();
+		checkGuess("tails", coin[0]);
+		animation(coin);
+	}
 
-heads.onclick = function() {
-	flipACoin(heads);
-	checkGuess("heads", coin[0]);
-	animation(coin);
-}
+	heads.onclick = function() {
+		flipACoin(heads);
+		checkGuess("heads", coin[0]);
+		animation(coin);
+	}
 
-flipButton.onclick = function() {
-	heads.style.display = "block";
-	tails.style.display = "block";
-	flipButton.style.display = "none";
-	message("");
-}
+	flipButton.onclick = function() {
+		heads.style.display = "block";
+		tails.style.display = "block";
+		flipButton.style.display = "none";
+		message("");
+	}
 
-const flipACoin = function() {
+	const flipACoin = function() {
 		let randomFlip = Math.random()
 		if (randomFlip < .5) {
 			coin = ["heads", "tails"];
@@ -36,7 +35,7 @@ const flipACoin = function() {
 		return coin;
 	}
 
-const checkGuess = function(headsOrTailsGuess, headsOrTailsRandom) {
+	const checkGuess = function(headsOrTailsGuess, headsOrTailsRandom) {
 		if (headsOrTailsGuess === headsOrTailsRandom) {
 			console.log("Your Guess: " + headsOrTailsGuess + " | Flip Result: " + headsOrTailsRandom);
 			window.setTimeout(function() {
@@ -52,30 +51,30 @@ const checkGuess = function(headsOrTailsGuess, headsOrTailsRandom) {
 		} else {
 			alert("something bad happened");
 		}
-}
-
-const animation = function(coin) {
-	coinSpin.style.display = "block";
-	tails.style.display = "none";
-	heads.style.display = "none";
-	if (coin[0] === "heads") {
-		window.setTimeout(function() {
-			coinSpin.style.display = "none";
-			heads.style.display = "block";
-			tails.style.display = "none";
-		}, 1500);
-	} else if (coin[0] === "tails") {
-		window.setTimeout(function() {
-			coinSpin.style.display = "none";
-			tails.style.display = "block";
-			heads.style.display = "none";
-		}, 1500);
-	} else {
-		alert("something bad happened");
 	}
-}
 
-const message = function(messageToDisplay) {
+	const animation = function(coin) {
+		coinSpin.style.display = "block";
+		tails.style.display = "none";
+		heads.style.display = "none";
+		if (coin[0] === "heads") {
+			window.setTimeout(function() {
+				coinSpin.style.display = "none";
+				heads.style.display = "block";
+				tails.style.display = "none";
+			}, 1500);
+		} else if (coin[0] === "tails") {
+			window.setTimeout(function() {
+				coinSpin.style.display = "none";
+				tails.style.display = "block";
+				heads.style.display = "none";
+			}, 1500);
+		} else {
+			alert("something bad happened");
+		}
+	}
+
+	const message = function(messageToDisplay) {
 		messageBox.innerHTML = messageToDisplay;
 	}
-
+}());
